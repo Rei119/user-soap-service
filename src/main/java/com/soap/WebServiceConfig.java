60c1de4ca9ec122053999a41f89da0a57ea561dd
Ori <orgil.nara@gmail.com>
2026-04-02 14:29:26 +0800

    fix soap endpoint for cloud

--- src/main/java/com/soap/WebServiceConfig.java
+++ src/main/java/com/soap/WebServiceConfig.java
@@ -1,5 +1,4 @@
 package com.soap;
-
 import jakarta.xml.ws.Endpoint;
 import org.springframework.context.annotation.Configuration;
 import jakarta.annotation.PostConstruct;
@@ -7,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 public class WebServiceConfig {
-
     @Autowired
     private AuthServiceImpl authServiceImpl;
 
     @PostConstruct
     public void publishEndpoint() {
-        Endpoint.publish("http://localhost:9002/auth", authServiceImpl);
+        String host = System.getenv("SOAP_HOST") != null 
+            ? System.getenv("SOAP_HOST") 
+            : "http://localhost:9002/auth";
+        Endpoint.publish(host, authServiceImpl);
     }
 }
